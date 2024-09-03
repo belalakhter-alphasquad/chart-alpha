247cdaba9850ce400886bbf1862ec31753c1aaa3
Bilal <bilalakhter.alphasquad@gmail.com>
2024-09-03 20:40:53 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>DEXTools Pair Explorer Widget Template</title>
+    </head>
+
+    <body>
+       <iframe id="dextools-widget"
+        title="DEXTools Trading Chart"
+        width="500"
+        height="400" src="https://www.dextools.io/widget-chart/en/ether/pe-light/0xa29fe6ef9592b5d408cca961d0fb9b1faf497d6d?theme=light&chartType=2&chartResolution=30&drawingToolbars=false"></iframe>
+    </body>
+    </html>
+  `;
+
+  return (
+    <div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div
+        className="html-content"
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </div>
+  );
+}
+
+export default App;
